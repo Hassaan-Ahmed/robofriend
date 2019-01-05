49d88243ae3b40175e9b89bcaff7d12689f5dfec
Hassaan-Ahmed <dashingsd@gmail.com>
2019-01-05 20:46:50 +0500

    added app list with filter working

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React,{ Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+    onSerachChange = (event) =>{     
+            this.setState({searchField: event.target.value});
+    }
+    render() {
+        const filteredResult = this.state.robots.filter((robo) => {
+            return robo.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+        return(
+            <div className="tc">
+                <h1>RoboFriend</h1>
+                <SearchBox searchChange={this.onSerachChange}/>
+                <CardList robots={filteredResult}/>
+            </div>
+        );
+    }
+  
+}
+export default App;
