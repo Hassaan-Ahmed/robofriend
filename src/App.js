60968f6fe952bc4cdf7f8345d3345c35496ede8c
Hassaan-Ahmed <dashingsd@gmail.com>
2019-01-06 11:46:54 +0500

    added data from web api

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import React,{ Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {this.setState({robots: users})});
+        // console.log('check');
     }
     onSerachChange = (event) =>{     
             this.setState({searchField: event.target.value});
     }
-    render() {
+  render() {
         const filteredResult = this.state.robots.filter((robo) => {
             return robo.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        return(
-            <div className="tc">
-                <h1 className="f1">RoboFriend</h1>
-                <SearchBox searchChange={this.onSerachChange}/>
-                <CardList robots={filteredResult}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className='tc'>Loading</h1>
+        }
+        else{        
+            return(
+                <div className="tc">
+                    <h1 className="f1">RoboFriend</h1>
+                    <SearchBox searchChange={this.onSerachChange}/>
+                    <Scroll>
+                        <CardList robots={filteredResult}/>
+                    </Scroll>
+                </div>
+            );
+        }
     }
   
 }
